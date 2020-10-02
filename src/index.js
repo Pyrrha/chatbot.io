627c419fef7d4e88df5d4b1449c5a7546768f1ce
Tristan Dietz <tristan.dietz@laposte.net>
2020-10-02 12:57:15 +0200

    Configuring basic material settings, adding logo, and bar, left pane (from Material documentation)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Chatbot from './Chatbot';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Chatbot />
   </React.StrictMode>,
   document.getElementById('root')
 );
